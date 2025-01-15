8d7eac2da2448bbe3072f4a7658401ca391397e1
zenon0777 <abderrahmane.daifi@protonmail.com>
2025-01-15 15:33:10 +0100

    Feat: Integrate PostHog for analytics tracking and update layout to include CSPostHogProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { CSPostHogProvider } from './providers'
 
 export const metadata: Metadata = {
   title: "ReserNova - Pitch Deck",
@@ -13,13 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
-      <body className="antialiased text-gray-900">
-        <SpeedInsights />
-        <div className="max-w-6xl lg:mx-auto p-2 md:p-6 lg:p-8">
-          {children}
-        </div>
-      </body  >
+    <html lang="en">
+      <CSPostHogProvider>
+
+        <body className="antialiased text-gray-900">
+          <SpeedInsights />
+          <div className="max-w-6xl lg:mx-auto p-2 md:p-6 lg:p-8">
+            {children}
+          </div>
+        </body  >
+      </CSPostHogProvider>
     </html>
   );
 }
