74ace13804ef8d19bd2c29fb7ff871cf741d74bd
zenon0777 <abderrahmane.daifi@protonmail.com>
2025-01-23 00:48:14 +0100

    Fix: Enable automatic pageview capture in PostHogProvider configuration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,8 +8,9 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+      capture_pageview: true // Disable automatic pageview capture, as we capture manually
       , capture_pageleave: true
+
     })
   }, [])
 
